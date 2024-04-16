447f00fe7e6329d617f2e8c84ed3c012799fb943
unknown <DAM110@iesataulfo.local>
2024-04-16 15:51:49 +0200

    Cuarta fase proyecto

--- src/directorioTrabajoGorostizaInigo/Clase.java
+++ src/directorioTrabajoGorostizaInigo/Clase.java
@@ -5,5 +5,9 @@ package directorioTrabajoGorostizaInigo;
  */
 public class Clase
 {
+	public Clase()
+	{
+		System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	}
 	// Esto es un comentario.
 }
