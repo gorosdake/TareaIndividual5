1112761531c1ad8420f1877c88a8ba3d14af5a31
unknown <DAM110@iesataulfo.local>
2024-04-16 15:42:53 +0200

    Segunda fase proyecto

--- src/directorioTrabajoGorostizaInigo/Principal.java
+++ src/directorioTrabajoGorostizaInigo/Principal.java
@@ -1,9 +1,16 @@
 package directorioTrabajoGorostizaInigo;
 
+/**
+ * Esta es la clase principal del proyecto
+ */
 public class Principal
 {
+	/**
+	 * Aqui esta la entrada del programa y su funcionalidad.
+	 * @param args Argumentos de llamada
+	 */
 	public static void main(String[] args)
 	{
-
+		// Esto es un comentario.
 	}
 }
