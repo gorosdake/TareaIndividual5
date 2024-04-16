c29593f03427be0d7a86b37c1fe3581b718916df
unknown <DAM110@iesataulfo.local>
2024-04-16 15:39:43 +0200

    Primera fase proyecto

---
+++ src/directorioTrabajoGorostizaInigo/Clase.java
@@ -0,0 +1,6 @@
+package directorioTrabajoGorostizaInigo;
+
+public class Clase
+{
+
+}
---
+++ src/directorioTrabajoGorostizaInigo/Principal.java
@@ -0,0 +1,9 @@
+package directorioTrabajoGorostizaInigo;
+
+public class Principal
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
